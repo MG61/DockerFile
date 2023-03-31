d29ad707a09a7ef2715c073cfd4783c4951bee8c
MG61 <85520381+MG61@users.noreply.github.com>
2023-03-31 18:38:27 +0300

    L

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import calendar
 
-
-print ('Добро пожаловать в календарь\n')
-
-
-year = int (input('Введите год: '))
-
-month = int (input('Введите номер месяца: '))
-
-
+print ('Календарь\n')
+int year = input('Введите год: '))
+int month = input('Введите месяца: '))
 print(calendar.month(year, month))
